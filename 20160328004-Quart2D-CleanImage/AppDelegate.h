97919cce0c9e70519b1d450b8a9182a9dfd66f85
raineryue <it_jinlei.yue@163.com>
2016-03-28 17:14:54 +0800

    Initial Commit

---
+++ 20160328004-Quart2D-CleanImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160328004-Quart2D-CleanImage
+//
+//  Created by Rainer on 16/3/28.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
